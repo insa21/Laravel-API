101af8ded82a29bd061d1efb22c050f92b91b34a
Indra Saepudin <indrasaepudin212@gmail.com>
2025-01-01 19:07:58 +0700

    Add data with API

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\BukuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('buku', [BukuController::class, 'index']);
+Route::get('buku/{id}', [BukuController::class, 'show']);
+Route::post('buku', [BukuController::class, 'store']);
+Route::put('buku/{id}', [BukuController::class, 'update']);
+Route::delete('buku/{id}', [BukuController::class, 'destroy']);
