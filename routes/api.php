7779eb2acf8f54a9c119d5228f09b35142fa35e1
Indra Saepudin <indrasaepudin212@gmail.com>
2025-01-04 00:38:41 +0700

    Keamanan menggunakan middleware

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,11 @@ Route::get('/user', function (Request $request) {
 
 
 
-Route::get('buku', [BukuController::class, 'index']);
-Route::get('buku/{id}', [BukuController::class, 'show']);
-Route::post('buku', [BukuController::class, 'store']);
-Route::put('buku/{id}', [BukuController::class, 'update']);
-Route::delete('buku/{id}', [BukuController::class, 'destroy']);
 
-
-// Route::apiResource('buku', [BukuController::class]);
+Route::middleware(['checkHost'])->group(function () {
+    Route::get('buku', [BukuController::class, 'index']);
+    Route::get('buku/{id}', [BukuController::class, 'show']);
+    Route::post('buku', [BukuController::class, 'store']);
+    Route::put('buku/{id}', [BukuController::class, 'update']);
+    Route::delete('buku/{id}', [BukuController::class, 'destroy']);
+});
